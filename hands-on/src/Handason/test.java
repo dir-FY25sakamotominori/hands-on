bb544d93092b094b40643a6e30e2b4e09a543e8a
dir-FY25sakamotominori <minori.sakamotoa281@dirbato.co.jp>
2025-05-23 16:39:15 +0900

    できてるかな

---
+++ hands-on/src/Handason/test.java
@@ -0,0 +1,14 @@
+package Handason;
+
+public class test {
+    //mainメソッドはプログラムの最初に動作します
+    public static void main(String[] args) {
+        /* comment out
+         System.out.println("Kiramex");
+        System.out.println("Java");
+        */
+        System.out.println("学習を始めましょう！");
+
+    }
+
+}
