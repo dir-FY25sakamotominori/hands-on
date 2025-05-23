19202a823e38fd2fdb149648bded3289be29ca25
dir-FY25sakamotominori <minori.sakamotoa281@dirbato.co.jp>
2025-05-23 17:32:35 +0900

    commit

---
+++ hands-on/src/Handason/Variable.java
@@ -0,0 +1,13 @@
+package Handason;
+
+public class Variable {
+
+    public static void main(String[] args){
+        var number1 = 1;
+        System.out.println(number1);
+        number1 = 2;
+        System.out.println(number1);
+        
+    }
+
+}
--- hands-on/src/Handason/test.java
+++ hands-on/src/Handason/test.java
@@ -2,7 +2,7 @@ package Handason;
 
 public class test {
     //mainメソッドはプログラムの最初に動作します
-    public static void main(String[] args) {
+    public static void main(String[] args){
         /* comment out
          System.out.println("Kiramex");
         System.out.println("Java");
